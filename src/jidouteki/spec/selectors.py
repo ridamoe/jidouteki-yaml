cc84653a159757be62bd3b327600d86b71c2d598
ll3006 <doublel3006@gmail.com>
2024-09-09 23:45:29 +0200

    Add json handling pipeline steps

--- src/jidouteki/spec/selectors.py
+++ src/jidouteki/spec/selectors.py
@@ -4,6 +4,7 @@ from jidouteki.spec.exceptions import PropertyTypeMismatch
 import re
 from typing import Literal
 import urllib.parse
+import json
 
 import typing
 if typing.TYPE_CHECKING:
@@ -11,10 +12,11 @@ if typing.TYPE_CHECKING:
 
 class Pipeline(ListSpec):
     def process(self, items_to_process: list):
+        saved = {}
         items = items_to_process
         for step in self:
             key = list(step.keys())
-            if len(key) > 1: raise PropertyTypeMismatch("Pipeline steps should only have one key")
+            if len(key) > 1: raise PropertyTypeMismatch(f"Pipeline steps should only have one key this has {len(key)}: {key}, {self}, {list(self)}")
             key = key[0]
             value = step[key]
             
@@ -29,9 +31,28 @@ class Pipeline(ListSpec):
                                  prop = item.get_text()
                             else:    
                                 prop = item.get(p, None)
-                            if not prop: continue
-                            return prop
-                        return None
+                        return prop
+                case 'prop':
+                    def _process(item):
+                        prop = None
+                        if value == "text" and callable(getattr(item, "get_text", None)):
+                                prop = item.get_text()
+                        else:    
+                            prop = item.get(value, None)
+                        return prop
+                case 'has':
+                    def _process(item):
+                        for requirements in value:
+                            req_key = requirements["key"]
+                            req_value = requirements["value"]
+                            if item.get(req_key) != req_value: return None 
+                        return item
+                case 'filter':
+                    assert isinstance(value, list)
+                    inner_pipeline =  Pipeline(value, _parent=self, _context=self._context)
+                    def _process(item):
+                        filtered = inner_pipeline.process([item])
+                        return item if filtered else None
                 case 'match':
                     self._root: Website
                     def _process(item):
@@ -47,7 +68,13 @@ class Pipeline(ListSpec):
                         return curr
                 case 'format':
                     def _process(item):
-                        return value.format(item)
+                        if isinstance(item, dict):
+                            return value.format(**item, **saved)
+                        elif isinstance(item, list):
+                            return value.format(*item, **saved)
+                        elif isinstance(item, str):
+                            print(item)
+                            return value.format(item, **saved)
                 case 'proxy':
                     def _process(item):
                         params = []
@@ -60,7 +87,15 @@ class Pipeline(ListSpec):
                         query_string = urllib.parse.urlencode(params)
                             
                         return f"{self._context.proxy}?{query_string}"
-
+                case 'reset':
+                    items = items_to_process
+                    continue
+                case 'save':
+                    if len(items) == 1:
+                        saved[value] = items[0]
+                    elif len(items) == 0:
+                        saved[value] = ""
+                    continue
                 case _: raise PropertyTypeMismatch(f"Unknown pipeline step {key}")
             
             items = [_process(item) for item in items]
@@ -93,6 +128,8 @@ class Selector(DictSpec):
             case 'regex':
                 matches.extend(re.findall(self.query, str(document)))
             case 'xpath': raise NotImplementedError()
+            case 'json':
+                matches.extend([json.loads(document.text)])
             case _: raise PropertyTypeMismatch(self.type)
         if self.pipeline:
             matches = self.pipeline.process(matches)
